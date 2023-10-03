ec1948ea581d27f942e0cdef85a48f25b049f592
PrajwalLuitel <prajwalluitel@gmai.com>
2023-10-03 16:33:37 +0545

    draggable and droppable elements added

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,11 +1,49 @@
-import { DragDropContext } from "react-beautiful-dnd";
+"use client";
 
-import React from 'react'
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+
+import React, { useEffect } from "react";
+import useBoardStore from "@/store/BoardStore";
+import Column from "./column";
 
 const Board = () => {
-  return (
-    <div>Board</div>
-  )
-}
+  const [ board, getBoard ] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
 
-export default Board
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+
+  const handleOnDragEnd = (result: DropResult) => {
+    
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd} >
+      <Droppable droppableId = "board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+          className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) =>(
+              <Column
+                key={id}
+                id={id}
+                todos={column.todos}
+                index={index}
+              />
+            ))}
+          </div>
+        )}
+
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default Board;
---
+++ store/BoardStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+import {getTodosGroupedByColumn}  from "@/utils/getTodosGroupedByColumn";
+
+
+interface BoardState{
+    board: Board;
+    getBoard: () => void;
+
+}
+
+const useBoardStore = create<BoardState>((set) => ({ 
+    board: {
+        columns: new Map<TypedColumn, Column>()
+    },
+    getBoard: async () => {
+        const board = await getTodosGroupedByColumn();
+        set({ board }); 
+    }
+}))
+export default useBoardStore;

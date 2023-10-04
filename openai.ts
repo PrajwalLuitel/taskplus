6c80f68bc3b0a80161b6299629bb64af763dc7c1
PrajwalLuitel <prajwalluitel@gmai.com>
2023-10-04 14:18:23 +0545

    api key added

--- openai.ts
+++ openai.ts
@@ -1,10 +1,10 @@
-import Configuration,{ OpenAI } from "openai";
+import  { OpenAI } from "openai";
 
 // const configuration = new Configuration({
 //   apiKey: process.env.OPENAI_API_KEY,
 // });
 // console.log("OpenAI API KEY: ", process.env.OPENAI_API_KEY);
 
-const openai = new OpenAI();
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export default openai;
